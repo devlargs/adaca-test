0fe3071110ed3f03c8365e4e76716b452bcfbff1
Ralph Largo <devlargs@gmail.com>
2025-02-10 22:35:29 +0800

    feat: added ui for displaying lyrics

---
+++ src/components/LyricsModal/LyricsModal.tsx
@@ -0,0 +1,43 @@
+import { fetcher } from "@/utils/fetcher";
+import { Box, Loader, Modal } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import { FC } from "react";
+import { ErrorNote } from "../ErrorNote";
+
+const LyricsModal: FC<{
+  opened: boolean;
+  onClose: () => void;
+  id?: number;
+  title: string;
+}> = ({ opened, onClose, id, title }) => {
+  const { error, data, isFetching, isPending } = useQuery({
+    queryKey: ["trackLyrics", id],
+    queryFn: async () => {
+      return fetcher(`/track.lyrics.get?track_id=${id}`);
+    },
+  });
+
+  return (
+    <>
+      <Modal opened={opened} onClose={onClose} title={title}>
+        <>
+          {isFetching || isPending ? (
+            <Loader size="sm" type="dots" />
+          ) : error ? (
+            <ErrorNote />
+          ) : (
+            <>
+              <Box
+                dangerouslySetInnerHTML={{
+                  __html: data?.message?.body?.lyrics?.lyrics_body,
+                }}
+              />
+            </>
+          )}
+        </>
+      </Modal>
+    </>
+  );
+};
+
+export default LyricsModal;
--- src/components/SongTable/SongTable.tsx
+++ src/components/SongTable/SongTable.tsx
@@ -1,8 +1,14 @@
 import { Track } from "@/types";
 import { Badge, Button, Table } from "@mantine/core";
-import { FC } from "react";
+import { useDisclosure } from "@mantine/hooks";
+import { FC, useState } from "react";
+import LyricsModal from "../LyricsModal/LyricsModal";
 
 export const SongTable: FC<{ tracks: Track[] }> = ({ tracks }) => {
+  const [title, setTitle] = useState<string>("");
+  const [trackId, setTrackId] = useState<number>();
+  const [opened, { open, close }] = useDisclosure(false);
+
   const rows = tracks.map((track) => (
     <Table.Tr key={track.track_id}>
       <Table.Td>{track.track_name}</Table.Td>
@@ -14,7 +20,16 @@ export const SongTable: FC<{ tracks: Track[] }> = ({ tracks }) => {
       </Table.Td>
       <Table.Td>
         {track.has_lyrics ? (
-          <Button variant="transparent" size="xs" radius="xl">
+          <Button
+            variant="transparent"
+            size="xs"
+            radius="xl"
+            onClick={() => {
+              setTitle(track?.track_name);
+              setTrackId(track?.track_id);
+              open();
+            }}
+          >
             Show Lyrics
           </Button>
         ) : null}
@@ -32,9 +47,13 @@ export const SongTable: FC<{ tracks: Track[] }> = ({ tracks }) => {
   );
 
   return (
-    <Table captionSide="bottom">
-      <Table.Thead>{ths}</Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
-    </Table>
+    <>
+      <Table captionSide="bottom">
+        <Table.Thead>{ths}</Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+
+      <LyricsModal title={title} id={trackId} onClose={close} opened={opened} />
+    </>
   );
 };
